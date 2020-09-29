2a5b52bfef8dcc882be0be55ce56f1a43737530b
kai-blt <blaitong@gmail.com>
2020-09-29 13:55:45 -0700

    more modifications to event listners

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -9,26 +9,8 @@ console.log(blockArray);
 //Add on click to each block which moves it to the top of the stack
 
 
-// let clicked = "";
-
-// window.addEventListener('click', e => {
-//     clicked = e.type;
-// })
-
-// // function checkClicks() {
-// //     console.log('checking...')
-// //     if (clicked === 'click') {          
-// //         console.log(clicked);
-// //         // gsap.to(block, {duration: 1, y: '-=50'});
-// //     };        
-// // }
-// // window.setInterval(checkClicks, 1000);
-
 
 Array.from(blockArray).forEach(block => {
-    block.addEventListener('click', (e) => {
-       blockContainer.prepend(block);
-    });
     block.addEventListener('mousedown', (e) => {
         console.log(e.target.className);
         setInterval(function(){
@@ -41,5 +23,9 @@ Array.from(blockArray).forEach(block => {
         setInterval(function(){
             gsap.to(block, {duration: 1, y: '-=50'});
         })    
-    })
+    })    
+    
+    block.addEventListener('click', (e) => {
+        blockContainer.prepend(block);
+     });
 });

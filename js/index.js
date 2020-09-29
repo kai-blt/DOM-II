0fcc75d5b1b591f8708614672327497b702fd9be
kai-blt <blaitong@gmail.com>
2020-09-28 18:28:54 -0700

    Added event listeners to all nav links, mouseover and mouseout

--- js/index.js
+++ js/index.js
@@ -9,3 +9,19 @@
 // * `select`
 // * `dblclick`
 // * `drag / drop`
+
+let navLinks = document.querySelectorAll('.nav-link');
+
+//Change link color to gold on mouseover
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', (e) => {
+        link.style.color = 'gold';
+    });
+});
+
+//Change link color to black on mouseout
+navLinks.forEach(link => {
+    link.addEventListener('mouseout', (e) => {
+        link.style.color = '#000';
+    });
+});

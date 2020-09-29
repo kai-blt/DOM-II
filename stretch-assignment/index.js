bab677ded99d8f59ba3942aea3fc56fbf72aaf38
kai-blt <blaitong@gmail.com>
2020-09-29 13:07:53 -0700

    added mouseup

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -9,17 +9,37 @@ console.log(blockArray);
 //Add on click to each block which moves it to the top of the stack
 
 
+// let clicked = "";
+
+// window.addEventListener('click', e => {
+//     clicked = e.type;
+// })
+
+// // function checkClicks() {
+// //     console.log('checking...')
+// //     if (clicked === 'click') {          
+// //         console.log(clicked);
+// //         // gsap.to(block, {duration: 1, y: '-=50'});
+// //     };        
+// // }
+// // window.setInterval(checkClicks, 1000);
+
 
 Array.from(blockArray).forEach(block => {
     block.addEventListener('click', (e) => {
        blockContainer.prepend(block);
     });
-    // block.addEventListener('click', (e) => {
-    //     gsap.to(block, {duration: 1, 'blockContainer.prepend(block)'});
-    // })
     block.addEventListener('mousedown', (e) => {
-      if (e.type === 'mousedown') {
-        gsap.to(block, {duration: 1, y: '+=50'});
-      };        
+        console.log(e.target.className);
+        setInterval(function(){
+            gsap.to(block, {duration: 1, y: '-=50'});   
+        });        
     });
+    
+    block.addEventListener('mouseup', (e) => {
+        console.log('UP');
+        setInterval(function(){
+            gsap.to(block, {duration: 1, y: '-=50'});
+        })    
+    })
 });
